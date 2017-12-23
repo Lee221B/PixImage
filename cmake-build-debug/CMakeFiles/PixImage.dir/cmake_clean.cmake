e4ae26feaed2574a1c763cc4194fd6690ea686e5
Lee221B <984884034@qq.com>
2017-12-23 17:48:10 +0800

    add cmake and directory generated

---
+++ cmake-build-debug/CMakeFiles/PixImage.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PixImage.dir/main.cpp.o"
+  "PixImage.pdb"
+  "PixImage"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PixImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
